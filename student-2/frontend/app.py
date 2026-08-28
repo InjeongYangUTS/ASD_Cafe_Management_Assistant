6a88546c8b780584efb53494ae78c34e7a1df6c3
Ei Thandar <eithandar@Eis-MacBook-Air.local>
2026-08-28 11:37:23 +1000

    Add Student 2 Frontend

---
+++ student-2/frontend/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/menus")
+def menus():
+    return render_template("menus.html")
+
+
+@app.route("/recipes")
+def recipes():
+    return render_template("recipes.html")
+
+
+@app.route("/ingredients")
+def ingredients():
+    return render_template("ingredients.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5200, debug=True)
